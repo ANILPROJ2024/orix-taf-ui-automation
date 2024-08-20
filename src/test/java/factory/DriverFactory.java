b6444be06eb2a136d3115be352382db3fcc299e7
UserANILPROJ2024 <anil.n@moolya>
2024-08-20 12:56:51 +0530

    add

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -2,11 +2,16 @@ package factory;
 
 import constants.Browser;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriverFactory {
 
@@ -16,16 +21,26 @@ public class DriverFactory {
 
         switch (Browser.valueOf(browser.toUpperCase())) {
             case CHROME -> {
+                Map<String, Object> prefs = new HashMap<String, Object>();
+                prefs.put("profile.default_content_setting_values.notifications", 1);
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--remote-allow-origins=*");
-                if (System.getProperty("browserMode", "normal").equalsIgnoreCase("headless")) {
-                    chromeOptions.addArguments("--headless");
-                    chromeOptions.addArguments("window-size=1920,1080");
-                }
-                if (System.getProperty("debugMode", "false").equalsIgnoreCase("true"))
-                    chromeOptions.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
+                chromeOptions.setExperimentalOption("prefs", prefs);
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("screen-resolution", "800x600");
+                chromeOptions.setExperimentalOption("prefs",
+                        Collections.singletonMap("autofill.credit_card_enabled", false));
+                chromeOptions.addArguments("--incognito");
+//                chromeOptions.addArguments("--headless");
+//                if (System.getProperty("browserMode", "normal").equalsIgnoreCase("headless")) {
+//                    chromeOptions.addArguments("--headless");
+//                    chromeOptions.addArguments("window-size=1920,1080");
+//                }
+//                if (System.getProperty("debugMode", "false").equalsIgnoreCase("true"))
+//                    chromeOptions.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
                 driver = new ChromeDriver(chromeOptions);
+
             }
             case FIREFOX -> {
                 WebDriverManager.firefoxdriver().setup();
@@ -37,7 +52,6 @@ public class DriverFactory {
             }
             default -> throw new RuntimeException("Invalid Browser: " + browser);
         }
-
         driver.manage().window().maximize();
         return driver;
     }
--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -9,17 +9,19 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import pages.LoginPage;
-import pages.MapPage;
+import pages.*;
 import utilities.ConfigLoader;
 import utilities.CredsLoader;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Hooks {
     private WebDriver driver;
     private final TestContext context;
+    private static Map<String, String> testData;
 
     public Hooks(TestContext context) {
         this.context = context;
@@ -32,8 +34,18 @@ public class Hooks {
         context.credsLoader = new CredsLoader();
         context.configLoader = new ConfigLoader();
         context.scenario = scenario;
-        context.loginPage = new LoginPage(context.driver);
-        context.mapPage = new MapPage(context.driver);
+        context.testData = new HashMap<>();
+        context.homePage = new HomePage(context.driver);
+        context.signUpPage = new SignUpPage(context.driver);
+        context.signInPage = new SignInPage(context.driver);
+        context.userHomePage = new UserHomePage(context.driver);
+        context.carsPage = new CarsPage(context.driver);
+        context.bookSummaryPage = new BookSummaryPage(context.driver);
+        context.customerinfoPage = new CustomerinfoPage(context.driver);
+        context.checkoutPage = new CheckoutPage(context.driver);
+        context.fleetsPage = new FleetsPage(context.driver);
+        context.myProfilePage = new MyProfilePage(context.driver);
+        context.bookingDetailsPage = new BookingDetailsPage(context.driver);
     }
 
     @After
@@ -45,8 +57,9 @@ public class Hooks {
             scenario.attach(fileContent, "image/png", "image");
         }
 
-        if (driver != null)
-            driver.quit();
+        if (driver != null) {
+        }
+        // driver.quit();
     }
 
 }
